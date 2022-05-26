7c71e02c31046037dc0ebf95b3e79f0b1f3ecc7f
Marchee72 <marche_871@hotmail.com>
2022-05-26 16:57:18 -0300

    errors

--- src/api/core/errors/unauthorized_error.go
+++ src/api/core/errors/unauthorized_error.go
@@ -1,1 +1,16 @@
 package errors
+
+type UnauthorizedError struct {
+	message string
+	cause   []string
+}
+
+func NewUnauthorizedError(message string) UnauthorizedError {
+	return UnauthorizedError{
+		message: message,
+	}
+}
+
+func (e UnauthorizedError) Error() string {
+	return e.message
+}
