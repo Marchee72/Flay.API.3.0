37a552c2a848aa8bd78a65dd0c280f823dfccb1f
Marchee72 <marche_871@hotmail.com>
2022-09-24 18:28:44 -0300

    usecase file

--- src/api/core/usecases/get_building_announcements/get_building_announcements.go
+++ src/api/core/usecases/get_building_announcements/get_building_announcements.go
@@ -1,1 +1,11 @@
 package get_building_announcements
+
+import (
+	"context"
+
+	"flay-api-v3.0/src/api/core/contracts/get_building_announcements"
+)
+
+type UseCase interface {
+	Execute(ctx context.Context, request get_building_announcements.Request)
+}
